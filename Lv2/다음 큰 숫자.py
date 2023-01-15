a916c61b8b9a883f0ebc87867be4ae702cde232f
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2023-01-15 21:01:30 +0900

    Update 다음 큰 숫자.py

--- Lv2/다음 큰 숫자.py
+++ Lv2/다음 큰 숫자.py
@@ -1,10 +1,10 @@
 def solution(n):
     answer = 0
     m = n
+    n_zero = bin(n).count('1')
     while True:
         m += 1
-        n_zero = str(bin(n))[2:].count('1')
-        m_zero = str(bin(m))[2:].count('1')
+        m_zero = bin(m).count('1')
         if n_zero == m_zero:
             break
     return m
